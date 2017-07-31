43bf76e3fdc9efe35f418af3b298e95828bb67a0
LLWhite <white800612@gmail.com>
2017-07-31 18:19:12 +0800

    add NPOI Excel import and Word export&import

--- NPOItest/Controllers/NPOIController.cs
+++ NPOItest/Controllers/NPOIController.cs
@@ -6,19 +6,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using NPOItest.Models.Sevices;
+using NPOI.XWPF.UserModel;
 
 namespace NPOItest.Controllers
 {
     public class NPOIController : Controller
     {
         private string fileSavedPath = "~/Content/";
-        private ExcelServices excelServices = new ExcelServices();
+        private NPOIServices NPServices = new NPOIServices();
 
-        // GET: NPOI
-        public void EmptyExport()
+        // GET: NPOI Excel
+        public void EmptyExport_E()
         {
             FileStream fs = new FileStream(string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp.xls"), FileMode.Open, FileAccess.ReadWrite);
-            HSSFWorkbook templateWorkbook = excelServices.AccountEmpty(fs);
+            HSSFWorkbook templateWorkbook = NPServices.AccountEmpty_E(fs);
 
             Response.Clear();
             Response.ContentType = "application/vnd.openxmlformats";
@@ -29,11 +30,11 @@ namespace NPOItest.Controllers
             Response.End();
         }
 
-        // GET: NPOI
-        public void DataExport()
+        // GET: NPOI Excel
+        public void DataExport_E()
         {
             FileStream fs = new FileStream(string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp.xls"), FileMode.Open, FileAccess.ReadWrite);
-            HSSFWorkbook templateWorkbook = excelServices.AccountData(fs);
+            HSSFWorkbook templateWorkbook = NPServices.AccountData_E(fs);
 
             Response.Clear();
             Response.ContentType = "application/vnd.openxmlformats";
@@ -44,17 +45,17 @@ namespace NPOItest.Controllers
             Response.End();
         }
 
-        // GET: NPOI
-        public void PDFExport(int SchoolCtrl001Id = 0)
+        // GET: NPOI Excel
+        public void PDFExport_E()
         {
             FileStream fs = new FileStream(string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp.xls"), FileMode.Open, FileAccess.ReadWrite);
 
-            HSSFWorkbook templateWorkbook = excelServices.AccountData(fs);
+            HSSFWorkbook templateWorkbook = NPServices.AccountData_E(fs);
 
             MemoryStream ms = new MemoryStream();
             templateWorkbook.Write(ms);
 
-            string target = string.Concat(Server.MapPath(fileSavedPath), "/temp/" + System.Guid.NewGuid().ToString() + "暫存EXCEL.xls");//??
+            string target = string.Concat(Server.MapPath(fileSavedPath), "/temp/" + System.Guid.NewGuid().ToString() + "EXCEL.xls");//??
             using (var fileStream = new FileStream(target, FileMode.CreateNew, FileAccess.ReadWrite))
             {
                 ms.Position = 0;
@@ -62,7 +63,7 @@ namespace NPOItest.Controllers
             }
 
             ConvertPDFHelper Convert = new ConvertPDFHelper();
-            string pdfPath = string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp/" + System.Guid.NewGuid().ToString() + "轉檔.pdf");
+            string pdfPath = string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp/" + System.Guid.NewGuid().ToString() + ".pdf");
             string PDFfile = Convert.ConvertExcelToPdf(target, pdfPath);
 
             Stream iStream = new FileStream(PDFfile, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
@@ -74,11 +75,142 @@ namespace NPOItest.Controllers
 
             Response.Clear();
             Response.ContentType = "application/vnd.openxmlformats";
-            Response.AddHeader("Content-Disposition", "attachment; filename=" + Server.UrlPathEncode("AccounrPDF.pdf"));
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Server.UrlPathEncode("AccountPDF.pdf"));
 
             memoryStream.WriteTo(Response.OutputStream);
 
             Response.End();
+        }
+
+        // POST: NPOI Excel
+        [HttpPost]
+        public ActionResult Import_E(HttpPostedFileBase file)
+        {
+            string message;
+            if (file != null && file.ContentLength > 0 && file.ContentLength < (10 * 1024 * 1024))
+            {
+                string filetype = file.FileName.Split('.').Last();
+                string fileName = Path.GetFileName(file.FileName);
+                string path = Path.Combine(Server.MapPath("~/Content/Imports"), fileName);
+                if (filetype == "xls")
+                {
+                    file.SaveAs(path);
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                    HSSFWorkbook excel = new HSSFWorkbook(fs);
+
+                    message = NPServices.InsertData_E(excel);
+                }
+                else
+                {
+                    message = "File format error !";
+                }
+            }
+            else
+            {
+                message = "Please select file import !";
+            }
+            ViewBag.Message = message;
+
+            return View();
+        }
+
+
+        // GET: NPOI Word
+        public void EmptyExport_W()
+        {
+            FileStream fs = new FileStream(string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp.docx"), FileMode.Open, FileAccess.ReadWrite);
+            XWPFDocument templateWorkbook = NPServices.AccountEmpty_W(fs);
+
+            Response.Clear();
+            Response.ContentType = "application/vnd.openxmlformats";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Server.UrlPathEncode("AccountEmpty.docx"));
+            MemoryStream ms = new MemoryStream();
+            templateWorkbook.Write(ms);
+            ms.WriteTo(Response.OutputStream);
+            Response.End();
+        }
+
+        // GET: NPOI Word
+        public void DataExport_W()
+        {
+            FileStream fs = new FileStream(string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp.docx"), FileMode.Open, FileAccess.ReadWrite);
+            XWPFDocument templateWorkbook = NPServices.AccountData_W(fs);
+
+            Response.Clear();
+            Response.ContentType = "application/vnd.openxmlformats";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Server.UrlPathEncode("AccountData.docx"));
+            MemoryStream ms = new MemoryStream();
+            templateWorkbook.Write(ms);
+            ms.WriteTo(Response.OutputStream);
+            Response.End();
+        }
+
+        // GET: NPOI Word
+        public void PDFExport_W()
+        {
+            FileStream fs = new FileStream(string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp.docx"), FileMode.Open, FileAccess.ReadWrite);
+            XWPFDocument templateWorkbook = NPServices.AccountData_W(fs);
+
+            MemoryStream ms = new MemoryStream();
+            templateWorkbook.Write(ms);
+
+            string target = string.Concat(Server.MapPath(fileSavedPath), "/temp/" + System.Guid.NewGuid().ToString() + "Word.docx");//??
+            using (var fileStream = new FileStream(target, FileMode.CreateNew, FileAccess.ReadWrite))
+            {
+                ms.Position = 0;
+                ms.CopyTo(fileStream); // fileStream is not populated
+            }
+
+            ConvertPDFHelper Convert = new ConvertPDFHelper();
+            string pdfPath = string.Concat(Server.MapPath(fileSavedPath), "/Excels/temp/" + System.Guid.NewGuid().ToString() + ".pdf");
+            string PDFfile = Convert.ConvertExcelToPdf(target, pdfPath);
+
+            Stream iStream = new FileStream(PDFfile, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            iStream.CopyTo(memoryStream);
+            iStream.Dispose();
+
+            Response.Clear();
+            Response.ContentType = "application/vnd.openxmlformats";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Server.UrlPathEncode("AccountPDF.pdf"));
+
+            memoryStream.WriteTo(Response.OutputStream);
+
+            Response.End();
+        }
+
+        // POST: NPOI Word
+        [HttpPost]
+        public ActionResult Import_W(HttpPostedFileBase file)
+        {
+            string message;
+            if (file != null && file.ContentLength > 0 && file.ContentLength < (10 * 1024 * 1024))
+            {
+                string filetype = file.FileName.Split('.').Last();
+                string fileName = Path.GetFileName(file.FileName);
+                string path = Path.Combine(Server.MapPath("~/Content/Imports"), fileName);
+                if (filetype == "docx")
+                {
+                    file.SaveAs(path);
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                    XWPFDocument word = new XWPFDocument(fs);
+
+                    message = NPServices.InsertData_W(word);
+                }
+                else
+                {
+                    message = "File format error !";
+                }
+            }
+            else
+            {
+                message = "Please select file import !";
+            }
+            ViewBag.Message = message;
+
+            return View();
         }
     }
 }
---
+++ NPOItest/Models/Sevices/NPOIServices.cs
@@ -0,0 +1,148 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace NPOItest.Models.Sevices
+{
+    public class NPOIServices
+    {
+        private NPOIModel db = new NPOIModel();
+
+        public HSSFWorkbook AccountEmpty_E(FileStream fs)
+        {
+            HSSFWorkbook templateWorkbook = new HSSFWorkbook(fs);
+            HSSFSheet ws = (HSSFSheet)templateWorkbook.GetSheetAt(0);
+
+            ws.GetRow(1).GetCell(2).SetCellValue(DateTime.Now.ToString("yyyy/MM/dd"));
+
+            return templateWorkbook;
+        }
+
+        public HSSFWorkbook AccountData_E(FileStream fs)
+        {
+            List<Account> data = db.Account.ToList();
+            HSSFWorkbook templateWorkbook = new HSSFWorkbook(fs);
+            HSSFSheet ws = (HSSFSheet)templateWorkbook.GetSheetAt(0);
+
+            ws.GetRow(1).GetCell(2).SetCellValue(DateTime.Now.ToString("yyyy/MM/dd"));
+
+            int startRow = 3;
+            int i = 1;
+            foreach (Account item in data)
+            {
+                ws.GetRow(startRow).GetCell(0).SetCellValue(i.ToString());
+                ws.GetRow(startRow).GetCell(1).SetCellValue(item.Username);
+                ws.GetRow(startRow).GetCell(2).SetCellValue(item.Name);
+                ws.GetRow(startRow).GetCell(3).SetCellValue(item.Email);
+                ws.GetRow(startRow).GetCell(4).SetCellValue(item.Sex);
+                ws.GetRow(startRow).GetCell(5).SetCellValue(item.Company);
+                ws.GetRow(startRow).GetCell(6).SetCellValue(item.Position);
+                ws.GetRow(startRow).GetCell(7).SetCellValue(item.Phone);
+                startRow++;
+                i++;
+            }
+            ws.ShiftRows(ws.LastRowNum + 1, ws.LastRowNum + (ws.LastRowNum - startRow + 1), -(ws.LastRowNum - startRow + 1));
+
+            return templateWorkbook;
+        }
+
+        public string InsertData_E(HSSFWorkbook excel)
+        {
+            HSSFSheet ws = (HSSFSheet)excel.GetSheetAt(0);
+            List<Account> newAccounts = new List<Account>();
+            int startRow = 3;
+            for (int i = startRow; i <= ws.LastRowNum; i++)
+            {
+                newAccounts.Add(new Account
+                {
+                    Username = ws.GetRow(startRow).GetCell(1).StringCellValue,
+                    Password = "520520",
+                    Name = ws.GetRow(startRow).GetCell(2).StringCellValue,
+                    Email = ws.GetRow(startRow).GetCell(3).StringCellValue,
+                    Sex = ws.GetRow(startRow).GetCell(4).StringCellValue,
+                    Company = ws.GetRow(startRow).GetCell(5).StringCellValue,
+                    Position = ws.GetRow(startRow).GetCell(6).StringCellValue,
+                    Phone = ws.GetRow(startRow).GetCell(7).StringCellValue
+                });
+                startRow++;
+            }
+            db.Account.AddRange(newAccounts);
+            db.SaveChanges();
+
+            return "Success !";
+        }
+
+        public XWPFDocument AccountEmpty_W(FileStream fs)
+        {
+            XWPFDocument templateWorkbook = new XWPFDocument(fs);
+            XWPFTable tb = templateWorkbook.Tables[0];
+
+            tb.GetRow(0).GetCell(1).SetText(DateTime.Now.ToString("yyyy/MM/dd"));
+
+            return templateWorkbook;
+        }
+
+        public XWPFDocument AccountData_W(FileStream fs)
+        {
+            List<Account> data = db.Account.ToList();
+            XWPFDocument templateWorkbook = new XWPFDocument(fs);
+            XWPFTable tb = templateWorkbook.Tables[0];
+
+            tb.GetRow(0).GetCell(1).SetText(DateTime.Now.ToString("yyyy/MM/dd"));
+            int startRow = 3;
+            int i = 1;
+            foreach (Account item in data)
+            {
+                tb.CreateRow().CreateCell();
+                tb.GetRow(startRow).GetCell(0).SetText(i.ToString());
+                tb.GetRow(startRow).GetCell(1).SetText(item.Username);
+                tb.GetRow(startRow).GetCell(2).SetText(item.Name);
+                tb.GetRow(startRow).CreateCell();
+                tb.GetRow(startRow).GetCell(3).SetText(item.Email);
+                tb.GetRow(startRow).CreateCell();
+                tb.GetRow(startRow).GetCell(4).SetText(item.Sex);
+                tb.GetRow(startRow).CreateCell();
+                tb.GetRow(startRow).GetCell(5).SetText(item.Company);
+                tb.GetRow(startRow).CreateCell();
+                tb.GetRow(startRow).GetCell(6).SetText(item.Position);
+                tb.GetRow(startRow).CreateCell();
+                tb.GetRow(startRow).GetCell(7).SetText(item.Phone);
+                startRow++;
+                i++;
+            }
+
+            return templateWorkbook;
+        }
+
+        public string InsertData_W(XWPFDocument word)
+        {
+            XWPFTable tb = word.Tables[0];
+            List<Account> newAccounts = new List<Account>();
+            int startRow = 3;
+            for (int i = startRow; i <= 5; i++)
+            {
+                newAccounts.Add(new Account
+                {
+                    Username = tb.GetRow(startRow).GetCell(1).GetText(),
+                    Password = "520520",
+                    Name = tb.GetRow(startRow).GetCell(2).GetText(),
+                    Email = tb.GetRow(startRow).GetCell(3).GetText(),
+                    Sex = tb.GetRow(startRow).GetCell(4).GetText(),
+                    Company = tb.GetRow(startRow).GetCell(5).GetText(),
+                    Position = tb.GetRow(startRow).GetCell(6).GetText(),
+                    Phone = tb.GetRow(startRow).GetCell(7).GetText()
+                });
+                startRow++;
+            }
+            db.Account.AddRange(newAccounts);
+            db.SaveChanges();
+
+            return "Success !";
+        }
+    }
+}
